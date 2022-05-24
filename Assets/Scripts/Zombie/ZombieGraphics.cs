b105b9e99313d81b45178bc5a3b63fc6285f753b
Edgar Lugo <10540123+edgarlug@users.noreply.github.com>
2022-05-24 18:29:53 -0400

    Added AI to follow Patient

---
+++ Assets/Scripts/Zombie/ZombieGraphics.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ZombieGraphics : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] AIPath aiPath;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("isRunning", ZombieHasHorizontalSpeed());
+        
+        FlipSprite();
+    }
+
+    void FlipSprite() {
+        if(ZombieHasHorizontalSpeed()) {
+            transform.localScale = new Vector2(Mathf.Sign(aiPath.desiredVelocity.x), 1f);
+        }
+    }
+
+    private bool ZombieHasHorizontalSpeed() {
+        return Mathf.Abs(aiPath.desiredVelocity.x) > Mathf.Epsilon;
+    }
+}
