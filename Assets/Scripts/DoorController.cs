26d7c451c0570300772405eb76d62b7415be044c
Edgar Lugo <10540123+edgarlug@users.noreply.github.com>
2022-05-29 15:12:37 -0400

    Added door sound

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
 
     [SerializeField] Sprite closedDoor;
     [SerializeField] Sprite openDoor;
     
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();    
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>(); 
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        audioSource.Play();
         spriteRenderer.sprite = openDoor;
     }
 
 
     private void OnTriggerExit2D(Collider2D other) {
+        audioSource.Play();
         spriteRenderer.sprite = closedDoor;
     }
 }
